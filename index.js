c012b194537482ddcd2378856729fee3b9d0148a
wahengchang <wahengchang2023@gmail.com>
2024-05-16 11:35:55 +0800

    added api router

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 
 app.use(express.static('public'))
 
+app.use('/api/assistant', require('./routers/assistant'));
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
